1a1076b93194c1dca5a2e0147c467971f5627e20
Zak Angelle <zaknotzak@gmail.com>
2016-01-30 10:10:35 -0600

    potato

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var gm = require('gm');
+var filename = process.argv[2];
+var filenameParts = filename.split('.');
+var filenameWithoutExtension = filenameParts[0];
+var filenameExtension = filenameParts[1];
+var output = filenameWithoutExtension + '-potatified.' + filenameExtension;
 
-gm('./bval.jpg').resize(150).resize(300).quality(7).write('./bval-potatified.jpg', function() {});
+gm(filename).size(function(err, size) {
+  var width = size.width;
+  gm(filename).resize(width/2).resize(width).quality(7).write(output, function() {});
+});
