4a65e4fcb79ff99c7eb932d241005dd1129aa6fa
Zak Angelle <zaknotzak@gmail.com>
2016-01-30 11:23:03 -0600

    potato

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var gm = require('gm');
+var Promise = require('bluebird');
 var filename = process.argv[2];
 var filenameParts = filename.split('.');
 var filenameWithoutExtension = filenameParts[0];
 var filenameExtension = filenameParts[1];
 var output = filenameWithoutExtension + '-potatified.' + filenameExtension;
 
-gm(filename).size(function(err, size) {
-  var width = size.width;
-  gm(filename).resize(width/2).resize(width).quality(7).write(output, function() {});
-});
+Promise.promisifyAll(gm.prototype);
+
+gm(filename).sizeAsync()
+  .then(function(size) {
+    var width = size.width;
+
+    return gm(filename)
+      .resize(width/2)
+      .resize(width)
+      .quality(7)
+      .writeAsync(output)
+  })
+  .then(function() {
+    console.log('potato');
+  });
