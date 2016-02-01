847974b0549987f2281f8cbf0915b19a3783b233
Zak Angelle <zaknotzak@gmail.com>
2016-01-31 20:38:35 -0600

    potato

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var gm = require('gm');
 var path = require('path');
-var Promise = require('bluebird');
 var express = require('express');
 var multer = require('multer');
 var mkdirp = require('mkdirp');
-var app = express();
+var potatify = require('./lib/potatify.js');
+var constants = require('./lib/constants.js');
 
-mkdirp('./uploads', function() {});
-mkdirp('./potatoes', function() {});
+mkdirp(constants.UPLOAD_DIR);
+mkdirp(constants.OUTPUT_DIR);
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, constants.UPLOAD_DIR)
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -19,40 +18,16 @@ var storage = multer.diskStorage({
 });
 
 var upload = multer({ storage: storage });
-
 var port = process.env.PORT || 8080;
-process.env.PWD = process.cwd();
+var app = express();
 
-app.use(express.static(process.env.PWD + '/static'));
+app.use(express.static(path.join(__dirname, '/static')));
 
-app.post('/potatify', upload.single('file'), function(req, res) {
+app.post('/potato', upload.single('file'), function(req, res) {
   potatify(req.file.path)
     .then(function(potatoPath) {
-      res.sendFile(__dirname + '/' + potatoPath);
+      res.sendFile(path.join(__dirname , potatoPath));
     });
 });
 
 app.listen(port);
-
-Promise.promisifyAll(gm.prototype);
-
-function potatify(filepath)
-{
-  var parsedFilepath = path.parse(filepath);
-  var outputFilename = parsedFilepath.name + '-potatified' + parsedFilepath.ext;
-  var outputFilepath = path.join('./potatoes', outputFilename);
-
-  return gm(filepath).sizeAsync()
-    .then(function(size) {
-      var width = size.width;
-
-      return gm(filepath)
-        .resize(width/2)
-        .resize(width)
-        .quality(7)
-        .writeAsync(outputFilepath);
-    })
-    .then(function() {
-      return outputFilepath;
-    });
-}
