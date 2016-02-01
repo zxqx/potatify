6757b490cae4f02f5cc011a727573ba2c09957cb
Zak Angelle <zaknotzak@gmail.com>
2016-01-31 21:24:10 -0600

    potato

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-var path = require('path');
-var express = require('express');
-var multer = require('multer');
-var mkdirp = require('mkdirp');
-var potatify = require('./lib/potatify.js');
-var constants = require('./lib/constants.js');
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+import mkdirp from 'mkdirp';
+import potatify from './lib/potatify.js';
+import constants from './lib/constants.js';
 
 mkdirp(constants.UPLOAD_DIR);
 mkdirp(constants.OUTPUT_DIR);
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, constants.UPLOAD_DIR)
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, constants.UPLOAD_DIR),
+  filename: (req, file, cb) => cb(null, file.originalname)
 });
 
-var upload = multer({ storage: storage });
-var port = process.env.PORT || 8080;
-var app = express();
+let upload = multer({ storage: storage });
+let port = process.env.PORT || 8080;
+let app = express();
 
 app.use(express.static(path.join(__dirname, '/static')));
 
-app.post('/potato', upload.single('file'), function(req, res) {
-  potatify(req.file.path)
-    .then(function(potatoPath) {
-      res.sendFile(path.join(__dirname , potatoPath));
-    });
+app.post('/potato', upload.single('file'), async(req, res) => {
+  let potatoPath = await potatify(req.file.path);
+  res.sendFile(path.join(__dirname, potatoPath));
 });
 
 app.listen(port);
